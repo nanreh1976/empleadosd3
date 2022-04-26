0830d2fa44cbf144496c04f4404ac8d7c7af0701
nanreh1976 <hernanchogomez@gmail.com>
2022-04-26 02:25:54 -0300

    directivas IV nomina de empleados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,28 @@ export class AppComponent {
 
 empleados:Empleado[]=[
 
-new Empleado("juan","Díaz","Presidente",7500),
-new Empleado("Ana","Martín","Directora",5500),
-new Empleado("María","Fdez","Jefa de Seccion",3500),
-new Empleado("Laura","Lopez","Administrativo",2500),
+//new Empleado("juan","Díaz","Presidente",7500),
+//new Empleado("Ana","Martín","Directora",5500),
+//new Empleado("María","Fdez","Jefa de Seccion",3500),
+//new Empleado("Laura","Lopez","Administrativo",2500),
+
 
 ];
+
+agregarEmpleado(){
+let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+this.empleados.push(miEmpleado);
+
+
+
+}
+
+
+
+cuadroNombre:string="";
+cuadroApellido:string="";
+cuadroCargo:string="";
+cuadroSalario:number=0;
+
+
 }
