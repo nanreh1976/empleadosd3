21c701d9c674487387b99c8991b46b3b2116f890
nanreh1976 <hernanchogomez@gmail.com>
2022-07-05 01:20:40 -0300

    comunicacion componentes direct3d

---
+++ src/app/empleado-hijo-c/empleado-hijo-c.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-c',
+  templateUrl: './empleado-hijo-c.component.html',
+  styleUrls: ['./empleado-hijo-c.component.css']
+})
+export class EmpleadoHijoCComponent implements OnInit {
+
+@Input() emplist:Empleado 
+@Input() ind:number
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
